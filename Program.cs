ae5b14e0e1629cccb9d190c573808ad52e373f70
Aseem <aseemanand2@gmail.com>
2020-09-27 22:54:31 +0530

    [Aseem] Added code for length calculation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Line Comparison Computation Program"+"\n");
+            Console.WriteLine("Enter the X and Y coordinates of end points of the line");
+            double pointX1= Convert.ToDouble(Console.ReadLine());
+            double pointY1= Convert.ToDouble(Console.ReadLine());
+            double pointX2= Convert.ToDouble(Console.ReadLine());
+            double pointY2 = Convert.ToDouble(Console.ReadLine());
+
+            double powerX = Math.Pow(pointX2 - pointX1, 2);
+            double powerY= Math.Pow(pointY2 - pointY1, 2);
+            double lineLength = Math.Sqrt(powerX + powerY);
+            Console.WriteLine("Length of line= "+lineLength);
         }
     }
 }
